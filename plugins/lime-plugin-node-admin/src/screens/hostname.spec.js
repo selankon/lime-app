fe3baa4d0fde58464a36b5040856a67ec85b0105
selankon <selankon@selankon.xyz>
2022-10-25 17:35:34 +0200

    chore(react-query): update mutation syntax

--- plugins/lime-plugin-node-admin/src/screens/hostname.spec.js
+++ plugins/lime-plugin-node-admin/src/screens/hostname.spec.js
@@ -1,20 +1,18 @@
-
-
-import { screen, fireEvent, cleanup, act } from '@testing-library/preact';
-import '@testing-library/jest-dom';
-import waitForExpect from 'wait-for-expect';
+import "@testing-library/jest-dom";
+import { act, cleanup, fireEvent, screen } from "@testing-library/preact";
+import waitForExpect from "wait-for-expect";
 
 import { getBoardData, getChangesNeedReboot, getSession } from "utils/api";
-import { render } from 'utils/test_utils';
-import queryCache from 'utils/queryCache';
+import queryCache from "utils/queryCache";
+import { render } from "utils/test_utils";
 
-import HostnamePage from './hostname';
-import { changeHostname } from '../nodeAdminApi';
+import { changeHostname } from "../nodeAdminApi";
+import HostnamePage from "./hostname";
 
 jest.mock("utils/api");
-jest.mock('../nodeAdminApi');
+jest.mock("../nodeAdminApi");
 
-describe('hostname config', () => {
+describe("hostname config", () => {
     beforeEach(() => {
         getBoardData.mockImplementation(async () => ({
             hostname: "node-hostname",
@@ -31,43 +29,43 @@ describe('hostname config', () => {
         act(() => queryCache.clear());
     });
 
-    it('lets you change the hostname', async () => {
+    it("lets you change the hostname", async () => {
         render(<HostnamePage />);
         const input = await screen.findByLabelText("Node Name");
-        fireEvent.input(input, { target: { value: 'new-hostname' } });
-        expect(input.value).toBe('new-hostname');
-        const saveButton = await screen.findByRole('button', { name: /save/i });
+        fireEvent.input(input, { target: { value: "new-hostname" } });
+        expect(input.value).toBe("new-hostname");
+        const saveButton = await screen.findByRole("button", { name: /save/i });
         fireEvent.click(saveButton);
         await waitForExpect(() => {
-            expect(changeHostname).toBeCalledWith('new-hostname');
+            expect(changeHostname).toHaveBeenCalledWith("new-hostname");
         });
-        expect(await screen.findByTestId('changes-need-reboot')).toBeVisible();
+        expect(await screen.findByTestId("changes-need-reboot")).toBeVisible();
     });
 
-    it('shows an error message when hostname length is less than 3', async () => {
+    it("shows an error message when hostname length is less than 3", async () => {
         render(<HostnamePage />);
         const input = await screen.findByLabelText(/node name/i);
-        fireEvent.input(input, { target: { value: 'fo' } });
-        expect(input.value).toBe('fo');
-        const saveButton = await screen.findByRole('button', { name: /save/i });
+        fireEvent.input(input, { target: { value: "fo" } });
+        expect(input.value).toBe("fo");
+        const saveButton = await screen.findByRole("button", { name: /save/i });
         fireEvent.click(saveButton);
         // await screen.findByText(/the name should have at least 3 characters/i)
         // expect(
         //     screen.getByText(/the name should have at least 3 characters/i)
         // ).toBeInTheDocument();
-        expect(changeHostname).not.toBeCalled();
+        expect(changeHostname).not.toHaveBeenCalled();
     });
 
-    it('slugifies users input', async () => {
+    it("slugifies users input", async () => {
         render(<HostnamePage />);
         const input = await screen.findByLabelText(/node name/i);
-        fireEvent.input(input, { target: { value: 'foo_foo foo' } });
-        expect(input.value).toBe('foo-foo-foo');
-        const saveButton = await screen.findByRole('button', { name: /save/i });
+        fireEvent.input(input, { target: { value: "foo_foo foo" } });
+        expect(input.value).toBe("foo-foo-foo");
+        const saveButton = await screen.findByRole("button", { name: /save/i });
         fireEvent.click(saveButton);
         await waitForExpect(() => {
-            expect(changeHostname).toBeCalledWith('foo-foo-foo');
+            expect(changeHostname).toHaveBeenCalledWith("foo-foo-foo");
         });
-        expect(await screen.findByTestId('changes-need-reboot')).toBeVisible();
+        expect(await screen.findByTestId("changes-need-reboot")).toBeVisible();
     });
 });
