6e15acddbc2355b14de3579170358ea70b3a4c85
selankon <selankon@selankon.xyz>
2024-09-17 16:19:15 +0200

    chore(meshwideupgrade): show scheduled countdown

--- plugins/lime-plugin-mesh-wide-upgrade/src/components/upgradeState/UpgradeScheduled.tsx
+++ plugins/lime-plugin-mesh-wide-upgrade/src/components/upgradeState/UpgradeScheduled.tsx
@@ -11,6 +11,7 @@ export const UpgradeScheduled = () => {
     const { totalNodes } = useMeshUpgrade();
     const { errors, results } = useParallelScheduleUpgrade();
     const nodesToBeUpgraded = results?.length;
+    const { thisNode } = useMeshUpgrade();
 
     return (
         <UpgradeState title={<Trans>Upgrade is scheduled!</Trans>}>
@@ -18,6 +19,14 @@ export const UpgradeScheduled = () => {
                 <Trans>
                     {nodesToBeUpgraded} of {totalNodes} will be upgraded
                 </Trans>
+                {thisNode?.safeupgrade_start_remaining && (
+                    <div>
+                        <Trans>
+                            Node will upgrade in
+                            {thisNode?.safeupgrade_start_remaining} seconds
+                        </Trans>
+                    </div>
+                )}
                 {errors?.length > 0 && <ParallelErrors errors={errors} />}
             </>
         </UpgradeState>
