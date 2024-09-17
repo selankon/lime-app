0b2e5f34568c1b8f740cf4d906b3693ebd777247
selankon <selankon@selankon.xyz>
2024-09-17 16:43:10 +0200

    chore(meshwideupgrade): implement empty shared state modal

--- plugins/lime-plugin-mesh-wide-upgrade/src/components/modals.tsx
+++ plugins/lime-plugin-mesh-wide-upgrade/src/components/modals.tsx
@@ -5,11 +5,11 @@ import { useCallback } from "react";
 import { useModal } from "components/Modal/Modal";
 
 import { useMeshWideUpgradeInfo } from "plugins/lime-plugin-mesh-wide-upgrade/src/meshUpgradeQueries";
+import { MeshWideUpgradeInfo } from "plugins/lime-plugin-mesh-wide-upgrade/src/meshUpgradeTypes";
 import {
     ConditionFun,
     abortCondition,
     confirmCondition,
-    getNodeIpsByCondition,
     getNodesByCondition,
     scheduleCondition,
 } from "plugins/lime-plugin-mesh-wide-upgrade/src/utils/api";
@@ -20,6 +20,7 @@ interface IUseParallelQueriesModalProps {
     title?: VNode;
     content?: VNode;
     btnTxt?: VNode;
+    condition: ConditionFun;
 }
 
 const useParallelQueriesModal = ({
@@ -28,32 +29,63 @@ const useParallelQueriesModal = ({
     title,
     content,
     btnTxt = <Trans>Schedule</Trans>,
+    condition,
 }: IUseParallelQueriesModalProps) => {
     const { toggleModal, setModalState } = useModal();
     const runAndClose = useCallback(() => {
         cb(null);
         toggleModal();
     }, [cb, toggleModal]);
+    const { data: nodes } = useMeshWideUpgradeInfo({});
+    const targets = getNodesByCondition(nodes, condition);
+
+    let _content = (
+        <div className={"flex flex-col gap-4"}>
+            {content}
+            <TargetNodes targets={targets.map(([nodeName]) => nodeName)} />
+        </div>
+    );
+    if (targets.length === 0) {
+        _content = (
+            <div className={"flex flex-col gap-4"}>
+                <Trans>
+                    There are no compatible nodes for this action on the shared
+                    state. Await until shared state is updated to perform this
+                    operation
+                </Trans>
+            </div>
+        );
+    }
 
     const showModal = useCallback(() => {
-        setModalState({
-            content,
-            title,
-            successCb: useSuccessBtn ? runAndClose : undefined,
-            deleteCb: !useSuccessBtn ? runAndClose : undefined,
-            successBtnText: btnTxt,
-            deleteBtnText: btnTxt,
-        });
+        if (targets.length === 0) {
+            setModalState({
+                content: _content,
+                title,
+                cancelBtn: true,
+            });
+        } else {
+            setModalState({
+                content: _content,
+                title,
+                successCb: useSuccessBtn ? runAndClose : undefined,
+                deleteCb: !useSuccessBtn ? runAndClose : undefined,
+                successBtnText: btnTxt,
+                deleteBtnText: btnTxt,
+            });
+        }
         toggleModal();
     }, [
+        targets.length,
         setModalState,
-        content,
+        _content,
         title,
         useSuccessBtn,
         runAndClose,
         btnTxt,
         toggleModal,
     ]);
+
     return { showModal, toggleModal };
 };
 
@@ -63,23 +95,15 @@ export const useScheduleUpgradeModal = ({
 }: IUseParallelQueriesModalProps) => {
     let title = <Trans>All nodes are ready</Trans>;
     let content = (
-        <div>
-            <Trans>
-                Schedule a firmware upgrade for all nodes on the network
-            </Trans>
-            <TargetNodes condition={scheduleCondition} />
-        </div>
+        <Trans>Schedule a firmware upgrade for all nodes on the network</Trans>
     );
     if (!useSuccessBtn) {
         title = <Trans>Some nodes are not ready</Trans>;
         content = (
-            <div>
-                <Trans>
-                    Are you sure you want to start mesh wide upgrade? <br />
-                    Check node list to see the network status
-                </Trans>
-                <TargetNodes condition={scheduleCondition} />
-            </div>
+            <Trans>
+                Are you sure you want to start mesh wide upgrade? <br />
+                Check node list to see the network status
+            </Trans>
         );
     }
 
@@ -88,6 +112,7 @@ export const useScheduleUpgradeModal = ({
         cb,
         title,
         content,
+        condition: scheduleCondition,
     });
 };
 
@@ -97,23 +122,15 @@ export const useConfirmModal = ({
 }: IUseParallelQueriesModalProps) => {
     let title = <Trans>All nodes are upgraded successfully</Trans>;
     let content = (
-        <div>
-            <Trans>
-                Confirm mesh wide upgrade for all nodes on the network
-            </Trans>
-            <TargetNodes condition={confirmCondition} />
-        </div>
+        <Trans>Confirm mesh wide upgrade for all nodes on the network</Trans>
     );
     if (!useSuccessBtn) {
         title = <Trans>Some nodes don't upgraded properly</Trans>;
         content = (
-            <div>
-                <Trans>
-                    Are you sure you want to confirm the upgrade? <br />
-                    Check node list to see the network status
-                </Trans>
-                <TargetNodes condition={abortCondition} />
-            </div>
+            <Trans>
+                Are you sure you want to confirm the upgrade? <br />
+                Check node list to see the network status
+            </Trans>
         );
     }
 
@@ -123,19 +140,17 @@ export const useConfirmModal = ({
         title,
         content,
         btnTxt: <Trans>Confirm</Trans>,
+        condition: confirmCondition,
     });
 };
 
 export const useAbortModal = ({ cb }: IUseParallelQueriesModalProps) => {
     const title = <Trans>Abort current mesh wide upgrade?</Trans>;
     const content = (
-        <div>
-            <Trans>
-                This will the abort current upgrade process on all nodes. Are
-                you sure you want to proceed?
-            </Trans>
-            <TargetNodes condition={abortCondition} />
-        </div>
+        <Trans>
+            This will the abort current upgrade process on all nodes. Are you
+            sure you want to proceed?
+        </Trans>
     );
     const btnTxt = <Trans>Abort</Trans>;
     return useParallelQueriesModal({
@@ -144,16 +159,11 @@ export const useAbortModal = ({ cb }: IUseParallelQueriesModalProps) => {
         title,
         content,
         btnTxt,
+        condition: abortCondition,
     });
 };
 
-const TargetNodes = ({ condition }: { condition: ConditionFun }) => {
-    const { data: nodes } = useMeshWideUpgradeInfo({});
-
-    const targets = getNodesByCondition(nodes, condition).map(
-        ([nodeName]) => nodeName
-    );
-
+const TargetNodes = ({ targets }: { targets: string[] }) => {
     return (
         <div>
             <Trans>
